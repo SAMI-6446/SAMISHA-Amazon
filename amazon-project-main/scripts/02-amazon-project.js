395a9f8b9d8cadd994e21ef15e64c86ba21f572c
samiAmazon <samigomena5@gmail.com>
2025-08-06 04:24:41 -0700

    add formating currency to amzon js

--- amazon-project-main/scripts/02-amazon-project.js
+++ amazon-project-main/scripts/02-amazon-project.js
@@ -6,7 +6,7 @@ import {
 import {
   products
 } from "../data/products.js";
-
+import { formatCurrency } from "./utils/money.js";
 let producthtml = "";
 products.forEach((product) => {
   producthtml += `
@@ -31,7 +31,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-price">
-              $${(product.priceCents / 100).toFixed(2)}
+              $${formatCurrency(product.priceCents)}
             </div>
 
             <div class="product-quantity-container">
