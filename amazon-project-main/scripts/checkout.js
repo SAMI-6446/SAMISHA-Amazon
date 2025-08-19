5c039e4015ff0a8291c854e0f373ba9a27459a5c
samiAmazon <samigomena5@gmail.com>
2025-08-19 06:27:16 -0700

    add backend practice file

--- amazon-project-main/scripts/checkout.js
+++ amazon-project-main/scripts/checkout.js
@@ -1,5 +1,5 @@
 import { renderOrderSummary } from './checkout/order-summery.js';
 import {renderPaymentSummary } from './checkout/paymentSummery.js';
-// import '../data/cart-class.js';
+import '../data/backend-practice.js'; 
 renderOrderSummary();
 renderPaymentSummary();
